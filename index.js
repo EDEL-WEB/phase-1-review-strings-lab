8cf8358389c1816227e3c00262c7593c1530fa35
edel <edel.omondi@student.moringaschool.com>
2025-03-04 15:13:19 +0300

    updated 1 file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
